6cf78c00fd952c9abb87e4b8ffaa895579e61905
Преслав Първанов <parvanovvvv@gmail.com>
2022-11-29 00:36:14 +0200

    DentalClinic.UnitTests

---
+++ DentalClinic.UnitTests/ApplicationDbContextTests.cs
@@ -0,0 +1,27 @@
+﻿using DentalClinic.DB.Data.Configuration;
+using DentalClinic.DB.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DentalClinic.UnitTests
+{
+    public class ApplicationDbContextTests : IdentityDbContext
+    {
+        public ApplicationDbContextTests()
+        {
+        }
+        public ApplicationDbContextTests(DbContextOptions<ApplicationDbContextTests> options)
+           : base(options)
+        {
+           
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS2019;Database=DentalClinic;Integrated Security=True;");
+            }
+        }
+        public DbSet<Doctor> Doctors { get; set; } = null!;
+    }
+}
---
+++ DentalClinic.UnitTests/UnitTests.DB/DoctorUnitTests.cs
@@ -0,0 +1,25 @@
+﻿
+using DentalClinic.DB.Common;
+using DentalClinic.DB.Data.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace DentalClinic.UnitTests.UnitTests.DB
+{
+    internal class DoctorUnitTests
+    {
+   
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_GetAllActiveDoctorCount()
+        {
+        
+
+            int count = 3;
+            Assert.AreEqual(count, 3);
+        }
+    }
+}
