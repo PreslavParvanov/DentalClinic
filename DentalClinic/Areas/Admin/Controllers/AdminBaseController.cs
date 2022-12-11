25a1ba3162a1b6598a5da2d14ff95155096838d2
Преслав Първанов <parvanovvvv@gmail.com>
2022-12-11 20:57:39 +0200

    Create Admin areas

---
+++ DentalClinic/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static DentalClinic.Areas.Admin.Constants.AdminConstants;
+
+namespace DentalClinic.Areas.Admin.Controllers
+{
+    [Area(AreaName)]
+    [Authorize(Roles = AdminRolleName)]
+    public class AdminBaseController : Controller
+    {
+    }
+}
