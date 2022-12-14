ad4055d923e688c13f7c8f1aa67168749ce106e9
Преслав Първанов <parvanovvvv@gmail.com>
2022-12-14 23:59:20 +0200

    add JS section in Reception

--- DentalClinic/Areas/Admin/Controllers/ReceptionController.cs
+++ DentalClinic/Areas/Admin/Controllers/ReceptionController.cs
@@ -22,24 +22,24 @@ namespace DentalClinic.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Reception(string Type, DateTime DateSearch, Guid DoctorId, string CustomerId, DateTime curenttDate)
+        public async Task<IActionResult> Reception(DateTime DateSearch)
         {
-            if (Type == "HDR")
-            {
-                TimeSpan startTime = new TimeSpan(00, 00, 00);
-                TimeSpan endTime = new TimeSpan(23, 59, 59);
-                DateTime startDate = DateSearch + startTime;
-                DateTime endDate = DateSearch + endTime;
-                var result = await resectionService.GetAllBookedByDate(startDate, endDate);
-                ViewBag.dateSearch = DateSearch;
-                return View("ReceptionHdr", result);
-            }
-            else
-            {
-                var result = await resectionService.GetBookedDetailsById(DoctorId, CustomerId, curenttDate);
-                return View("ReceptionDet", result);
-            }
 
+            TimeSpan startTime = new TimeSpan(00, 00, 00);
+            TimeSpan endTime = new TimeSpan(23, 59, 59);
+            DateTime startDate = DateSearch + startTime;
+            DateTime endDate = DateSearch + endTime;
+            var result = await resectionService.GetAllBookedByDate(startDate, endDate);
+            ViewBag.dateSearch = DateSearch;
+            return View("ReceptionHdr", result);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ReceptionDet(Guid DoctorId, string CustomerId, DateTime curenttDate)
+        {
+            var result = await resectionService.GetBookedDetailsById(DoctorId, CustomerId, curenttDate);
+            return View("ReceptionDet", result);
         }
     }
 }
